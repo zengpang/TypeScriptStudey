a646bb91b954af63d7da6582958806c840699617
曾胖 <2516629927@qq.com>
2023-03-26 11:37:49 +0800

    更新TypeScript初次学习

---
+++ helloWorld/helloWorld.js
@@ -0,0 +1,6 @@
+(function () {
+    function sayHi(str) {
+        console.log("\u4F60\u597D ".concat(str));
+    }
+    sayHi('123');
+})();
---
+++ helloWorld/helloWorld.ts
@@ -0,0 +1,6 @@
+(()=>{
+    function sayHi(str:String){
+        console.log(`你好 ${str}`)
+    }
+    sayHi('123');
+})()
